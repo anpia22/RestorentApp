6f13c99cdf4f8657afe6ec3093c90656bb2516e9
Anup Ashtankar <122741245+anpia22@users.noreply.github.com>
2023-06-23 15:30:03 +0530

    Add files via upload

---
+++ src/components/Layout/LittleSummary.js
@@ -0,0 +1,17 @@
+import './LittleSummary.css';
+
+const LittleSummary = () => {
+    return ( <div className="summary">
+        <h2>Good Food, Good Taste</h2>
+        <p>
+            Every good item and every food items mast be good
+            and we are glad to surve you the best dishes to you.
+        </p>
+        <p>
+            let's try some of our marvalous delishous paradise. try it!!...
+            Good is Always Be Good.  
+        </p>
+    </div> );
+}
+ 
+export default LittleSummary;
