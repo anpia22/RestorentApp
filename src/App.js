ccdaa76fbb37728e24cb0e7281716c022767f206
Anup Ashtankar <122741245+anpia22@users.noreply.github.com>
2023-06-23 15:01:42 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
-import cartimg from './Images/pngwing.com.png';
-import headerimg from './Images/healthyfactor-meals.jpg';
-import './App.css';
+import { Fragment } from "react";
+import Header from "./components/Layout/Header";
+
 function App() {
   return (
-    <div>
-      <div className='header'>
-        <h2>ReactMeals</h2>
-        <div className='cart-section'>
-          <img src={cartimg} alt='cart'/>
-          Your cart
-          <span>0</span>
-        </div>
-      </div>
-      <img className='header-img' src={headerimg} alt='header-img'/>
-    </div>
+    <Fragment>
+      <Header />
+    </Fragment>
   );
 }
 
