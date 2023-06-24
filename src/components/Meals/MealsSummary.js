7b43e6bcc2b6c6d95f5749db72e9169da9676175
Anup Ashtankar <122741245+anpia22@users.noreply.github.com>
2023-06-24 15:22:45 +0530

    Add files via upload

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,18 @@
+import './MealsSummary.css'
+const MealsSummary = () => {
+    return ( 
+        <section className="summary">
+            <h2>Good Food, Good Taste</h2>
+        <p>
+            Every good item and every food items mast be good
+            and we are glad to surve you the best dishes to you.
+        </p>
+        <p>
+            let's try some of our marvalous delishous paradise. try it!!...
+            Good is Always Be Good.  
+        </p>
+        </section>
+     );
+}
+ 
+export default MealsSummary;
