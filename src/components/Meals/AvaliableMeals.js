199689e8437f33c8137680da92544398aa6b79b7
Anup Ashtankar <122741245+anpia22@users.noreply.github.com>
2023-06-25 15:40:48 +0530

    Update AvaliableMeals.js

--- src/components/Meals/AvaliableMeals.js
+++ src/components/Meals/AvaliableMeals.js
@@ -32,6 +32,7 @@ const DUMMY_MEALS = [
 const AvaliableMeals = () => {
     const mealsList = DUMMY_MEALS.map((meals => (<MealItem
         key={meals.id}
+        id = {meals.id}
         name={meals.name}
         description={meals.description}
         price={meals.price} />)))
