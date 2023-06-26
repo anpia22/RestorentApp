5751ac8e1d0088e3163849bd4a02ed8ea6734a8b
Anup Ashtankar <122741245+anpia22@users.noreply.github.com>
2023-06-27 02:02:37 +0530

    Update Cart.js

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,22 +1,25 @@
 import './Cart.css'
 import Modal from '../UI/Modal';
 
-
-const Cart = () => {
-    const cartItem = (<ul>
-        {[{id: 'm1', name: 'Sushi', amount: 2, price: 22.99, }].map((item)=>(<li>{item.name}</li>))}
-    </ul>)
-    return ( <Modal>
-        {cartItem}
-        <div className="total">
-            <span>Total Amount</span>
-            <span>35.22</span>
-        </div>
-        <div className="actions">
-            <button className="buttonAlt">Close</button>
-            <button className="button">Order</button>
-        </div>
-    </Modal> );
+const Cart = (props) => {
+    const cartItems = (<ul className='cart-items'>
+        {
+            [{ id: 'm1', name: 'Sushi', amount: 2, price: 22.99, },].map((item) => (<li>{item.name}</li>))
+        }
+    </ul>);
+    return (
+        <Modal onClose={props.onClose} >
+            {cartItems}
+            <div className='total'>
+                <span>Total Amount</span>
+                <span>35.22</span>
+            </div>
+            <div className='actions'>
+                <button className='buttonAlt' onClick={props.onClose}>Close</button>
+                <button className='button'>Order</button>
+            </div>
+        </Modal>
+    );
 }
- 
+
 export default Cart;
