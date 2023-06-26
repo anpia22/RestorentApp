459d30e4d5e2888376cb0d190e7569a41e52f1f1
Anup Ashtankar <122741245+anpia22@users.noreply.github.com>
2023-06-27 02:01:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,20 +4,18 @@ import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 
 function App() {
-  // const [cartIsShown,setCartIsShown]= useState(false);
+  const [cartIsShown,setCartIsShown]= useState(false);
 
-  // const shownCartHandler = () => {
-  //   setCartIsShown(true)
-  // }
-  // const hideCartHandler = () => {
-  //   setCartIsShown(false)
-  // }
+  const shownCartHandler = () => {
+    setCartIsShown(true)
+  }
+  const hideCartHandler = () => {
+    setCartIsShown(false)
+  }
   return (
     <Fragment>
-      {/* {cartIsShown && <Cart onClose={hideCartHandler}/>}
-      <Header onShowCart={shownCartHandler} /> */}
-      <Cart/>
-      <Header/>
+      {cartIsShown && <Cart onClose={hideCartHandler}/>}
+      <Header onShowCart={shownCartHandler} />
       <main>
         <Meals />
       </main>
